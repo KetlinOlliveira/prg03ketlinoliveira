8871875da31d4b9ba9f02482ec9148e88fdffbf3
Ketlin Oliveira <ketlinkii2004@gmail.com>
2025-11-09 20:07:49 -0300

    Atividade11

---
+++ Atividade12/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import br.com.ifba.curso.view.TelaPrincipal;
+
+/**
+ *
+ * @author ketli
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+         TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+        tela.setResizable(false);
+        tela.setSize(400,300);
+    }
+}
